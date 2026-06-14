ec188d5a193eb4596eec16d215d807aedcee03aa
augustoSamu <samuca.augusto05@gmail.com>
2026-06-13 22:56:13 -0300

    feat: finalize 04

--- 04 - Hold Shift and Check Checkboxes/script.js
+++ 04 - Hold Shift and Check Checkboxes/script.js
@@ -1,1 +1,26 @@
-console.log('ola')
+const checkboxes = Array.from(
+  document.querySelectorAll("input[type='checkbox']"),
+);
+
+let lastChecked;
+
+function handleClick(e) {
+  let inBetween = false;
+  if (e.shiftKey && this.checked) {
+    checkboxes.forEach((checkbox) => {
+      if (checkbox === this || checkbox === lastChecked) {
+        inBetween = !inBetween;
+      }
+
+      if (inBetween) {
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  lastChecked = this;
+}
+
+checkboxes.forEach((checkbox) =>
+  checkbox.addEventListener("click", handleClick),
+);
