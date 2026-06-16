9b032e69a9b43912832665d8354ca1a928e3ecc9
augustoSamu <samuca.augusto05@gmail.com>
2026-06-15 21:57:45 -0300

    feat: finalize 05

--- 05 - Key Sequence Detection/script.js
+++ 05 - Key Sequence Detection/script.js
@@ -1,1 +1,22 @@
-// cornify_add();
+const currentKeys = [];
+const secretWord = "banana";
+
+document.addEventListener("keyup", (e) => {
+  currentKeys.push(e.key);
+
+  const word = currentKeys.join("");
+  verifyCode(word);
+});
+
+function verifyCode(word) {
+  if (word.length < secretWord.length) {
+    return;
+  }
+
+  if (word === secretWord) {
+    cornify_add();
+    currentKeys = [];
+  } else {
+    currentKeys.shift();
+  }
+}
